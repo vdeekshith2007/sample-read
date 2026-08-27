3c4cb456522038b1d6042dce4bf6b2818f81691e
vdeekshith2007 <vataparthideekshith18@gmail.com>
2026-08-27 21:20:05 +0530

    boonce

--- java/Basic/myProject/src/oops/EmployeeBonusSystem.java
+++ java/Basic/myProject/src/oops/EmployeeBonusSystem.java
@@ -28,9 +28,9 @@ public class EmployeeBonusSystem {
 
 
     public static void main(String[] args) {
-        employee e1 = new employee("Rajesh",50000);
-        employee e2 = new employee("Sanjog",60000);
-        employee e3 = new employee("Suraj",70000);
+        employee e1 = new employee("deekshith",50000);
+        employee e2 = new employee("guru",60000);
+        employee e3 = new employee("vataparthi ",70000);
         System.out.println(e1.calculateBonus(5));
         System.out.println(e2.calculateBonus(4));
         System.out.println(e3.calculateBonus(1));
